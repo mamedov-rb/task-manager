5e8c5e32c9b9ddd80ef227b5abdace046cace04d
Rustam Mamedov <rustam.mamedov.mail@gmail.com>
2019-10-05 22:36:59 +0300

    Added: basic auth.

--- back-end/src/main/java/ru/rmamedov/taskmanager/config/SecurityConfig.java
+++ back-end/src/main/java/ru/rmamedov/taskmanager/config/SecurityConfig.java
@@ -47,7 +47,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and().csrf().disable();
+                .cors()
+                .and()
+                .csrf().disable()
+                .httpBasic();
 
         http
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
