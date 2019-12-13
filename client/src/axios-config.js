b909daebda2f4b3cd7cca11c3546826430a80c17
Rustam <rustam.mamedov.mail@gmail.com>
2019-12-14 02:32:51 +0300

    some fix for run on server

--- client/src/axios-config.js
+++ client/src/axios-config.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios'
-const URL = process.env.API_BASE_URL || 'http://5.228.51.196:3000/api'
+const URL = process.env.API_BASE_URL || 'http://5.228.51.196:4000/api'
 const jwt = sessionStorage.getItem('jwt')
 
 export default axios.create({
