222cf55ee07c5de0a2a3aebfee35e40ed6533c24
Ubuntu <ubuntu@ip-172-31-28-31.us-east-2.compute.internal>
2020-04-05 16:31:05 +0000

    fixed postgres-container variables

--- client/src/axios-config.js
+++ client/src/axios-config.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios'
-const URL = process.env.API_BASE_URL || 'http://5.228.51.196:4000/api'
+const URL = process.env.API_BASE_URL || 'http://ubuntu@ec2-18-223-214-231.us-east-2.compute.amazonaws.com:4000/api'
 const jwt = sessionStorage.getItem('jwt')
 
 export default axios.create({
